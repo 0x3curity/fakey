3d57e22bc2f36fd8d0531c9e2f95a6d39b006b08
0x3curity <39014228+0x3curity@users.noreply.github.com>
2018-06-05 19:36:36 -0700

    Create fakeanon.sh

---
+++ fakeanon.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+##Fuck You Commander Snitch####
+####CommanderFakeAnon##########
+###<3 Samba/Xecurity/Dillinger#####
+#-----------------------------------
+
+function print_thefag()
+{
+	cat<<"EOT"
+		Hiya CommanderX, hows the fam?
+		Holly A Doyon, Jason E Doyon, Michael R Doyon, Abigail N Doyon, Angela M Doyon Jr, Catherine D Doyon, Edward J Doyon, Edward James Doyon, Jason E Doyon, Natasha R Doyon
+		--------
+		Still here bro,
+		3122 Larkdale Ct
+		San Diego, CA 92123 ?
+
+		Mind if we call ya back,
+		(386) 755-9411, (904) 693-0823?
+		-------------------------------
+		Guess those don't work, damn it..
+		Maybe we'll try emailing you?
+		cdoyon@earthlink.net, cdoyon@juno.com, lildoy@yahoo.com - still there?
+		------------------------------
+		Well we tried, maybe your family would know how we can correct this problem of slandering?
+
+		Holly right,
+		2321 SE 144th St
+		Starke, FL 32091
+		(904) 619-5212/(904) 368-0540
+		hadoyon@msn.com
+		hollydoyon7@peoplepc.com
+		hollydoyon7@yahoo.com
+		hdoyon@aol.com
+		hadoyon@yahoo.com
+		hdoyon@comcast.net
+		hdoyon@sbcglobal.net
+		hdoyon@frontiernet.net?
+		----------
+		Maybe Jason can help us..
+		13813 Sea Anchor Ln
+		Jacksonville Beach, FL 32250, right?
+
+		Hrm..no answer, maybe Michael can help us?
+		1738 Nolan Rd
+		Middleburg, FL 32068-3054
+		(904) 415-1234, (904) 415-2308, (619) 212-5212
+		michael.doyon@yahoo.com
+		mdoy39@aol.com
+		------------
+		Wow, they must be really busy. We'll try 2-3 more, then call it a day;
+
+		Natasha is it?
+		135 Joseph Cir
+		Glennville, GA 30427-5522
+		(503) 521-6608, (912) 545-9930
+		natashalee2011@gmail.com
+		.
+		...hmm, nothing, maybe Angela will know (last one):
+		10855 Indies Dr N
+		Jacksonville, FL 32246-8842
+		(904) 693-6571, (904) 264-3016
+		angelapisanijones@comcast.net
+		angela2goth@yahoo.com
+		angelamorgandoyon@live.com
+		angelagoth@yahoo.com
+		-----------------
+		Well, regardless if anyone chose to speak to us, I do believe you get our point Christopher.
+EOT
+}
+print_thefag
